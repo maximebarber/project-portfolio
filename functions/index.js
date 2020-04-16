a9e2b7e051aa264bf20de48de55002919d51cb06
Maxime Barber <maximebarber@gmail.com>
2020-04-16 17:50:10 +0200

    #38

--- functions/index.js
+++ functions/index.js
@@ -8,12 +8,12 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
     response.send("Hello from Firebase test!");
 });
 
-const createNotification = (notification) => {
-    return admin
+const createNotification = async (notification) => {
+    const document = await admin
         .firestore()
         .collection("notifications")
-        .add(notification)
-        .then((document) => console.log("Notification added: ", document));
+        .add(notification);
+    return console.log("Notification added: ", document);
 };
 
 exports.projectCreated = functions.firestore
@@ -29,3 +29,16 @@ exports.projectCreated = functions.firestore
     });
 
 // https://firebase.google.com/docs/functions/firestore-events?authuser=0#event_triggers
+
+exports.userJoined = functions.auth.user().onCreate(async (user) => {
+    const doc = await admin.firestore().collection("users").doc(user.uid).get();
+    const newUser = doc.data();
+    const notification = {
+        content: "New user created",
+        user: `${newUser.firstName} ${newUser.lastName}`,
+        time: admin.firestore.FieldValue.serverTimestamp(),
+    };
+    return createNotification(notification);
+});
+
+// https://firebase.google.com/docs/functions/auth-events?authuser=0
